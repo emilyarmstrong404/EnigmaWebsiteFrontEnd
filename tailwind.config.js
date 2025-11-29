23acfece16dd6eab8ebb1abf384979fe7fe0f768
Emily Armstrong <emarmstrong2@gmail.com>
2025-11-29 14:56:51 +0000

    config file

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './app/**/*.{js,ts,jsx,tsx}',   // For Next.js App Router
+      './ui/**/*.{js,ts,jsx,tsx}', // Your components
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
